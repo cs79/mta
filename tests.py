2200f52c31aaf623a9e8fc11d9fedd91dd6290e2
Alex <cloudstrife79@gmail.com>
2018-05-01 15:53:52 -0400

    ratchet tests

--- tests.py
+++ tests.py
@@ -3,10 +3,12 @@
 # activate py36
 import pandas_datareader.data as web
 import os, datetime
+from sklearn.linear_model import LinearRegression
 
 PATH = 'C:/Users/cloud/Dropbox/Projects/mta'
 os.chdir(PATH)
 from afe import *
+from deepar import *
 
 start = datetime.datetime(2000,1,1)
 end   = datetime.datetime(2017,12,31)
@@ -16,3 +18,10 @@ f     = web.DataReader('F', 'morningstar', start, end)
 c = check_input(f)
 
 t = build_features(f)
+
+r = Ratchet()
+mod = LinearRegression()
+r.model = mod
+r.df = t
+r.get_val_set()
+r.set_time_grid(n = 10)
